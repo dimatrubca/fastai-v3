a61cab650e4f07ac229ca73bd249ec2e9306f86a
Dmitri Trubca <32422633+dimatrubca@users.noreply.github.com>
2020-08-03 16:14:08 +0300

    Update server.py
    customized for my project

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1A4yqP96WlHzdDXz9s9Y0-LS5d3eG16o0'
+export_file_name = 'cat_sa_export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['awake', 'sleeping']
 path = Path(__file__).parent
 
 app = Starlette()
